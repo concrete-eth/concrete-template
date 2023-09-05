ab55f2281518433cc2c87aef3b566ef1b4eee76f
bytes <therealbyt3s@gmail.com>
2023-09-05 10:12:49 +0200

    refactor: rename counter interface

--- sol/ICounter.sol
+++ sol/ICounter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-interface Counter {
+interface ICounter {
     function number() external view returns (uint256);
 
     function setNumber(uint256 newNumber) external;
