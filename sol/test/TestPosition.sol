7478998dea9ebe7d67e1dc9e2232169e3cf24999
Fishon Amos <43862685+fishonamos@users.noreply.github.com>
2024-06-01 22:43:46 +0100

    test counter

--- sol/test/TestPosition.sol
+++ sol/test/TestPosition.sol
@@ -1,18 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../Position.sol";
+import "../counter_contract.sol";
 import "../codegen/PositionPrecompile.sol";
 
-contract TestPosition {
-    function setUp() public {}
-    
-    function testPosition() public {
-        Coord memory coord;
-        coord = PositionPrecompile.getPosition(0);
-        require(coord.x == 0 && coord.y == 0, "Position should be (0, 0)");
-        PositionPrecompile.setPosition(0, Coord(1, 2));
-        coord = PositionPrecompile.getPosition(0);
-        require(coord.x == 1 && coord.y == 2, "Position should be (1, 2)");
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+
+contract TestCounter {
+    Counter counter = Counter(DeployedAddresses.Counter());
+
+    function testInitialCount() public {
+        uint256 expected = 0;
+        Assert.equal(counter.getCount(), expected, "Initial count should be 0");
+    }
+
+    function testIncrement() public {
+        counter.increment();
+        uint256 expected = 1;
+        Assert.equal(counter.getCount(), expected, "Count should be 1 after increment");
     }
 }
+
